016db6376b8f186085268046d3e6af1ebf6a2131
Sambhava <sambhava@admins-MacBook-Pro.local>
2022-01-21 12:39:37 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { createContext } from "react";
+import "./App.css";
+import CompA from "./CompA";
 
-function App() {
+const FirstName = createContext();
+const LastName = createContext();
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div>hi</div>
   );
-}
+};
 
 export default App;
+export { FirstName, LastName };
